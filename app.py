33b62b5a1198cfa954654b56d503c7cba5e25c88
IDGS-805-22002434 <leaguehalo86@gmail.com>
2026-01-24 08:25:27 -0600

    operasbas

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+#El app.r sirve para crear este formato rapido, y siempre es necesario para crear una pagina,
+#los returns son necesarios para las funciones (def)
+@app.route("/")
+def index():
+    titulo="Flask IDGS805"
+    lista=["Juan","Pedro","Mario"]
+    return render_template("index.html", titulo=titulo, lista=lista)
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template("alumnos.html")
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html")
+
+@app.route("/hola")
+def hola():
+    return "<h1>----Hola Nuevo----</h1>"
+
+@app.route("/user/<int:id>/<string:name>")
+def user(id, name):
+    return "ID: {} Nombre: {}".format(id,name)
+
+@app.route("/formulario")
+def formulario():
+    return '''
+    <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+       
+        <label for="name">Paterno:</label>
+        <input type="text" id="name" name="name" required>
+    </form>
+'''
+
+@app.route("/operasBas",methods=['GET','POST'])
+def operasBas():
+    res=0
+    if request.method=="POST":
+        n1=request.form.get("n1")
+        n2=request.form.get("n2")
+        res = int(n1) + int(n2)
+        
+    return render_template("operasBas.html",res=res)
+
+@app.route("/resultado",methods=['GET','POST'])
+def resultado():
+    n1=request.form.get("n1")
+    n2=request.form.get("n2")
+    res = int(n1) + int(n2)
+    return "La suma de {} + {} = {}".format(n1,n2,res)
+
+if __name__ =='__main__':
+    app.run(debug=True)
